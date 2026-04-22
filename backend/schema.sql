af09c69bf631ff060d1211342edad290f87d71bd
Raylee1012 <1012.ray.lee@gmail.com>
2026-04-22 22:34:41 +0800

    update schema: add coins column

--- backend/schema.sql
+++ backend/schema.sql
@@ -3,5 +3,6 @@ CREATE TABLE users (
   custom_id text UNIQUE NOT NULL,
   email text UNIQUE NOT NULL,
   is_verified bool DEFAULT false,
+  coins integer DEFAULT 0,
   created_at timestamptz DEFAULT now()
 );
